6ac5035d86a600d01e7c235a24073b2481616deb
Georgie Horth <georgiehorth@gmail.com>
2020-11-02 15:43:19 +0000

    Balance tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,4 +3,7 @@ class Oystercard
   def initialize
   @balance = 0
   end
+  def top_up(value)
+    @balance += value
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,13 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  describe '#top_up' do
+    it 'responds with 1 argument' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+    it 'adds money to the card' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+  end
+
 end
