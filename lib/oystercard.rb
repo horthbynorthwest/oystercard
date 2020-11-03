f6c4e1decef0745a890907ad2d92d6521f84adec
Georgie Horth <georgiehorth@gmail.com>
2020-11-03 15:37:47 +0000

    touch_out method now charges minimum fare

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,31 +1,43 @@
 class Oystercard
   MAXIMUM_BALANCE = 90
-
+  MINIMUM_FARE = 1
   attr_reader :balance
 
   def initialize
   @balance = 0
+  @in_use = false
   end
 
   def top_up(value)
-    fail "Top up limit reached" if (@balance + value) > MAXIMUM_BALANCE
+    check_limit(value)
     @balance += value
   end
 
-  def deduct(value)
-    @balance -= value
-  end
-
   def touch_in
-    fail 'Insufficent Funds' if @balance < 1
+    check_for_minimum
     @in_use = true
   end
 
   def touch_out
+    deduct(MINIMUM_FARE)
     @in_use = false
   end
 
   def in_journey?
     @in_use
+  end
+
+  private
+
+  def deduct(value)
+    @balance -= value
+  end
+
+  def check_for_minimum
+    fail 'Insufficent Funds' if @balance < MINIMUM_FARE
+  end
+
+  def check_limit(value)
+    fail "Top up limit reached" if (@balance + value) > MAXIMUM_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -21,16 +21,17 @@ describe Oystercard do
     end
   end
 
-  describe '#deduct' do
-    it 'responds with 1 argument' do
-      expect(subject).to respond_to(:deduct).with(1).argument
-    end
-
-    it 'deducts money from the card' do
-      subject.top_up(2)
-      expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
-    end
-  end
+# no longer needed
+  #describe '#deduct' do
+  #   it 'responds with 1 argument' do
+  #     expect(subject).to respond_to(:deduct).with(1).argument
+  #   end
+  #
+  #   it 'deducts money from the card' do
+  #     subject.top_up(2)
+  #     expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
+  #   end
+  # end
 
   describe '#in_journey?' do
     it 'is initially not in a journey' do
@@ -42,7 +43,7 @@ describe Oystercard do
       expect(subject).to be_in_journey
     end
     it 'can touch out' do
-      subject.top_up(5)
+      subject.top_up(5) # adding value so no error raised. see line 54
       subject.touch_in #making in journey = true
       subject.touch_out #making in journey = false
       expect(subject).not_to be_in_journey
@@ -54,5 +55,12 @@ describe Oystercard do
       expect { subject.touch_in}.to raise_error 'Insufficent Funds'
     end
 
+    describe '#charging for journey' do
+      it 'on touch out the user is charged the minimum fair' do
+        subject.top_up(5)
+        subject.touch_in
+        expect { subject.touch_out }.to change{ subject.balance }.by -Oystercard::MINIMUM_FARE
+      end
+    end
   end
 end
